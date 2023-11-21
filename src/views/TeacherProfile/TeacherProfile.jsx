5e642c3a201db4e12ec90db5239d565e8df4f0c8
janaiv <j.tasheva90@gmail.com>
2023-11-21 22:16:47 +0200

    Update the TeacherProfile component to improve the user interface. Implement responsive design for image resizing to ensure compatibility across devices. Add a glowing effect to the header using the Lobster font to enhance visual appeal. Adjust layout to ensure alignment and cohesion with the search bar and action buttons.
    - Use Chakra UI responsive styles for dynamic image sizing
    - Apply a neon glow effect to the heading with CSS text-shadow
    - Ensure proper alignment of search bar and buttons with the image
    - Include the Lobster font for a more engaging header style

--- src/views/TeacherProfile/TeacherProfile.jsx
+++ src/views/TeacherProfile/TeacherProfile.jsx
@@ -1,49 +1,29 @@
-import {
-    Box,
-    Button,
-    Heading,
-    Input,
-    SimpleGrid,
-    Text,
-    VStack,
-    Image,
-    Center,
-  } from '@chakra-ui/react';
-  import quizImage from '../../assets/quiz.png';
-  
-  const TeacherProfile = () => {
-    return (
-      <Box
-        p={200}
-      >
-        <VStack spacing={4} align="stretch">
-          <Center>
-            <Heading as="h1">Welcome, [Teacher Name]</Heading>
-          </Center>
-          <Center>
-            <Input placeholder="Search Quizzes..." />
-          </Center>
-          <Center>
-            <SimpleGrid columns={3} spacing={10}>
-              {/* Replace these with actual quiz topic data */}
-              <Box>
-                <Center>
-                  <Image src={quizImage} />
-                </Center>
-                <Text textAlign="center">Math</Text>
-              </Box>
-              {/* Repeat for other topics */}
-            </SimpleGrid>
-          </Center>
-          <Center>
-            <Button colorScheme="blue">Live Quiz</Button>
-            <Button colorScheme="green">Create Quiz</Button>
-            <Button colorScheme="teal">Send Invitation</Button>
-          </Center>
-        </VStack>
-      </Box>
-    );
-  };
-  
-  export default TeacherProfile;
-  
+import { Box, Flex, Heading, Input, Button, VStack, Image } from '@chakra-ui/react';
+import quizTimeImage from '../../assets/quiz_time.png';
+import './TeacherProfile.css';
+
+const TeacherProfile = () => {
+  return (
+    <Box pt={{ base: '100px', md: '120px' }} px={{ base: '5', md: '10' }}>
+      <Flex direction="column" align="center" maxWidth="1200px" margin="0 auto">
+        <Heading as="h1" size="xl" mb={6} textAlign="center" className="glowing-heading" style={{ fontFamily: "'Lobster', cursive" }}>
+          Welcome, Teacher Name
+        </Heading>
+        <Flex align="center" justify="center" width="full">
+          <Box mr={10}> {/* You can adjust the margin as needed */}
+          <Image src={quizTimeImage} className="floating-image" />
+
+          </Box>
+          <VStack spacing={4} align="stretch" width="full" maxW="lg"> {/* Adjust the width as needed */}
+            <Input placeholder="Search Quizzes..." size="lg" />
+            <Button colorScheme="blue" isFullWidth>Live Quiz</Button>
+            <Button colorScheme="green" isFullWidth>Create Quiz</Button>
+            <Button colorScheme="teal" isFullWidth>Send Invitation</Button>
+          </VStack>
+        </Flex>
+      </Flex>
+    </Box>
+  );
+};
+
+export default TeacherProfile;
