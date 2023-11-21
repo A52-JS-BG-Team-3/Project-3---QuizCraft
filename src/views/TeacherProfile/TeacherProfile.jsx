0df442200cfc2e214016b0a2b8414618b0af4688
janaiv <j.tasheva90@gmail.com>
2023-11-21 18:59:22 +0200

    feat: Enhanced Teacher Profile UI and Integrated Quiz Page with API
    - Revamped Teacher Profile layout using Chakra UI components for better user experience.
    - Implemented dynamic background image functionality for a more personalized look.
    - Centered and resized the main image on the Home page with subtle floating animation for visual appeal.
    - Developed Quiz page with API connectivity for fetching and displaying quiz data.
    - Ensured responsive design and cross-browser compatibility for all new features.
    - Conducted thorough testing to ensure functionality and visual consistency across components.

--- src/views/TeacherProfile/TeacherProfile.jsx
+++ src/views/TeacherProfile/TeacherProfile.jsx
@@ -1,46 +1,49 @@
-
 import {
-  Box,
-  Button,
-  Heading,
-  Input,
-  SimpleGrid,
-  Text,
-  VStack,
-  Image,
-  Center
-} from '@chakra-ui/react';
-
-const TeacherProfile = () => {
-  return (
-    <Box p={5}>
-      <VStack spacing={4} align="stretch">
-        <Box>
-          <Heading as="h1">Welcome, [Teacher Name]</Heading>
-        </Box>
-        <Box>
-          <Input placeholder="Search Quizzes..." />
-        </Box>
-        <Box>
-          <SimpleGrid columns={3} spacing={10}>
-            {/* Replace these with actual quiz topic data */}
-            <Box>
-              <Center>
-                <Image src="[Quiz Topic Icon URL]" boxSize="50px" />
-              </Center>
-              <Text textAlign="center">Math</Text>
-            </Box>
-            {/* Repeat for other topics */}
-          </SimpleGrid>
-        </Box>
-        <Box>
-          <Button colorScheme="blue">Live Quiz</Button>
-          <Button colorScheme="green">Create Quiz</Button>
-          <Button colorScheme="teal">Send Invitation</Button>
-        </Box>
-      </VStack>
-    </Box>
-  );
-};
-
-export default TeacherProfile;
+    Box,
+    Button,
+    Heading,
+    Input,
+    SimpleGrid,
+    Text,
+    VStack,
+    Image,
+    Center,
+  } from '@chakra-ui/react';
+  import quizImage from '../../assets/quiz.png';
+  
+  const TeacherProfile = () => {
+    return (
+      <Box
+        p={200}
+      >
+        <VStack spacing={4} align="stretch">
+          <Center>
+            <Heading as="h1">Welcome, [Teacher Name]</Heading>
+          </Center>
+          <Center>
+            <Input placeholder="Search Quizzes..." />
+          </Center>
+          <Center>
+            <SimpleGrid columns={3} spacing={10}>
+              {/* Replace these with actual quiz topic data */}
+              <Box>
+                <Center>
+                  <Image src={quizImage} />
+                </Center>
+                <Text textAlign="center">Math</Text>
+              </Box>
+              {/* Repeat for other topics */}
+            </SimpleGrid>
+          </Center>
+          <Center>
+            <Button colorScheme="blue">Live Quiz</Button>
+            <Button colorScheme="green">Create Quiz</Button>
+            <Button colorScheme="teal">Send Invitation</Button>
+          </Center>
+        </VStack>
+      </Box>
+    );
+  };
+  
+  export default TeacherProfile;
+  
