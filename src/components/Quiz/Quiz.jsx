bb884eb966943ea932729b7f5bde9d50df189bff
janaiv <j.tasheva90@gmail.com>
2023-11-22 15:41:49 +0200

    feat: integrated Open Trivia DB API and added new images to assets
    Implemented the Open Trivia Database API to fetch quiz categories dynamically and display them in a dropdown menu. Added multiple images to the assets directory to enhance the UI visuals of quiz topics.

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -2,60 +2,63 @@ import { useState, useEffect } from 'react';
 import { Box, Button, Text } from '@chakra-ui/react';
 
 const Quiz = () => {
-  const [questions, setQuestions] = useState(null); // Инициализирайте с null
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [score, setScore] = useState(0);
-  const [quizFinished, setQuizFinished] = useState(false);
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [token, setToken] = useState('');
 
+  // Fetch categories on component mount
   useEffect(() => {
-    const fetchQuestions = async () => {
+    const fetchCategories = async () => {
       try {
-        const response = await fetch('https://opentdb.com/api.php?amount=10&type=multiple');
+        const response = await fetch('https://opentdb.com/api_category.php');
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
         const data = await response.json();
-        setQuestions(data.results);
+        setCategories(data.trivia_categories);
       } catch (error) {
-        console.error("Failed to fetch questions:", error);
+        console.error("Failed to fetch categories:", error);
       }
     };
 
-    fetchQuestions();
+    fetchCategories();
   }, []);
 
-  if (!questions) { // Проверка дали въпросите са заредени
-    return <Text>Loading questions...</Text>;
-  }
+  // Fetch a new session token
+  useEffect(() => {
+    const fetchToken = async () => {
+      try {
+        const response = await fetch('https://opentdb.com/api_token.php?command=request');
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        if (data.response_code === 0) {
+          setToken(data.token);
+        }
+      } catch (error) {
+        console.error("Failed to fetch session token:", error);
+      }
+    };
 
-  const handleAnswer = (selectedAnswer) => {
-    const correctAnswer = questions[currentQuestionIndex].correct_answer;
-    if (selectedAnswer === correctAnswer) {
-      setScore(score + 1);
-    }
-    if (currentQuestionIndex < questions.length - 1) {
-      setCurrentQuestionIndex(currentQuestionIndex + 1);
-    } else {
-      setQuizFinished(true);
-    }
+    fetchToken();
+  }, []);
+
+  const startQuiz = async (categoryId) => {
+    setSelectedCategory(categoryId);
+    // Here you would implement fetching questions for the selected category using the session token
+    // Make sure to handle the different response codes as per the API documentation
   };
 
   return (
     <Box>
-      {quizFinished ? (
-        <Text>Congratulations, you have finished the quiz! Your score: {score}</Text>
-      ) : (
-        <Box>
-          <Text>{questions[currentQuestionIndex].question}</Text>
-            {questions[currentQuestionIndex].incorrect_answers.concat(questions[currentQuestionIndex].correct_answer)
-              .sort(() => Math.random() - 0.5)
-              .map((answer, index) => (
-                <Button key={index} onClick={() => handleAnswer(answer)} m={2}>
-                  {answer}
-                </Button>
-              ))}
-          </Box>
-      )}
+      {categories.map((category) => (
+        <Button key={category.id} onClick={() => startQuiz(category.id)} m={2}>
+          {category.name}
+        </Button>
+      ))}
+      {/* Render quiz questions and handle quiz logic here */}
+      {/* Use the selectedCategory and token state variables to fetch and handle questions */}
     </Box>
   );
 };
